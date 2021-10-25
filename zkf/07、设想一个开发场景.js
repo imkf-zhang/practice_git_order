a4a8397d585b0d5841c8ccdeac996ed9b5d48df0
imkf-zhang <15638452918@163.com>
2021-10-25 22:35:04 +0800

    [功能]两个人共同在一个分支上开发的场景-->代验证

---
+++ zkf/07、设想一个开发场景.js
@@ -0,0 +1,9 @@
+// 两个开发人员：zcg 和 zkf  。   都在master分支上进行开发
+
+// 那么在zkf push代码之前可以这样
+
+// 1、先git add .    git commit -m    git push  
+// 然后再git pull 把远程代码更新到本地文件，因为远程代码，可能还有zcg开发的功能
+
+// 2、先git add .    git commit -m   然后再git pull  更新本地的代码，
+// 然后再git push 提交自己的代码。
