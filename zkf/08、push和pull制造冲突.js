ba58f1d11e1530d353230ae7ba3124a0e7fe59b0
imkf <63987718+imkf-zhang@users.noreply.github.com>
2022-03-05 14:29:22 +0800

    Update 08、push和pull制造冲突.js

--- zkf/08、push和pull制造冲突.js
+++ zkf/08、push和pull制造冲突.js
@@ -3,6 +3,6 @@
 // 直接在远程仓库改了  07文件  2222222222222222222222222223535
 //  然后在本地创建了08文件  进行一个提交
 function name(params) {
-    console.log(params1222222222)
+    console.log(param1222222222)
     console.log(params1)
 }
